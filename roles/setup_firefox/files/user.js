f0562ed3cb33926f17ccac9248afb1806de495fa
deus-ex-silicium <9478653+deus-ex-silicium@users.noreply.github.com>
2020-11-19 03:57:27 -0500

    removed firefox role

--- roles/setup_firefox/files/user.js
+++
@@ -1,4 +0,0 @@
-user_pref("browser.link.open_newwindow.restriction", 0);
-user_pref("network.IDN_show_punycode", true);
-user_pref("network.captive-portal-service.enabled", false);
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
